8a4e050f01ae26c236317b5de39491e2fa17a405
eracle <eracle@posteo.eu>
2018-11-10 17:48:34 +0100

    added todo, added requirement, added tagme sketch

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # semantic_annotation
 Arrichimento semantico degli Atti di OpenArs 
+
+
+
+# TODO:
+- modulo query SPARQL
+- finire l'interfaccia con TAGME
+- generatore URI dbpedia
+- salvataggio csv file, formato: uri_atto, start, end, dbpedia_uri
---
+++ annotator.py
@@ -0,0 +1,15 @@
+import tagme
+text = "Ciao come stai, questa è l'assemblea regionale siciliana"
+
+test_annotations = tagme.annotate(text, lang='it')
+
+    entities = []
+    annotations_dict = []
+
+    cre_count = 0
+    for annotation in test_annotations.get_annotations():
+        annotation_dic = vars(annotation)
+        entity_dic = dict(
+            id=annotation_dic.pop('entity_id'),
+            title=annotation_dic.pop('entity_title')
+        )
